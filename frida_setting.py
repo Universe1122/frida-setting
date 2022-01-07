46314fd8a30ab82b029aafa6e6db370f4b9777d1
Universe1122 <whdals7979@gmail.com>
2022-01-07 13:21:43 +0900

    init

---
+++ frida_setting.py
@@ -0,0 +1,25 @@
+import frida
+
+class frida_setting:
+    def __init__(self):
+        self.process = ''
+        self.hook_package_name = ''
+        self.script = []
+
+    def start(self, hook_package_name):
+        self.hook_package_name = hook_package_name
+
+        device = frida.get_usb_device(timeout=10)
+        pid = device.spawn([self.hook_package_name])
+        self.process = device.attach(pid)
+        device.resume(pid)
+    
+    def startScript(self, script):
+        self.script.append(self.process.create_script(script))
+        self.script[len(self.script) - 1].on("message", self.debugMessage)
+        self.script[len(self.script) - 1].load()
+
+        print(self.script)
+    
+    def debugMessage(self, message, data):
+        print("{} -> {}".format(message, data))
