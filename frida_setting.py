994114cb7e938451c591262b25b4d7f5d0a75e03
Universe1122 <whdals7979@gmail.com>
2022-01-07 13:26:20 +0900

    타입 추가

--- frida_setting.py
+++ frida_setting.py
@@ -6,7 +6,7 @@ class frida_setting:
         self.hook_package_name = ''
         self.script = []
 
-    def start(self, hook_package_name):
+    def start(self, hook_package_name: str):
         self.hook_package_name = hook_package_name
 
         device = frida.get_usb_device(timeout=10)
@@ -14,12 +14,10 @@ class frida_setting:
         self.process = device.attach(pid)
         device.resume(pid)
     
-    def startScript(self, script):
+    def startScript(self, script: str):
         self.script.append(self.process.create_script(script))
         self.script[len(self.script) - 1].on("message", self.debugMessage)
         self.script[len(self.script) - 1].load()
-
-        print(self.script)
     
     def debugMessage(self, message, data):
         print("{} -> {}".format(message, data))
